19bd7ed83d6c8ec2e6e26cef17edef9ecc98e2d7
Alexis Menendez <alexis.menendez@austincc.edu>
2025-04-26 16:36:13 -0500

    create auth-routes.ts

--- server/src/routes/index.ts
+++ server/src/routes/index.ts
@@ -1,9 +1,14 @@
 import { Router } from 'express';
 const router = Router();
 
+import authRoutes from './auth-routes.js';
 import apiRoutes from './api/index.js';
+import { authenticateToken } from '../middleware/auth.js';
 
-router.use('/api', apiRoutes);
+router.use('/api', authenticateToken, apiRoutes);
+
+router.use('/auth', authRoutes);
+
 
 router.use((_req, res) => {
   return res.send('404 Not Found: The requested resource does not exist.');
