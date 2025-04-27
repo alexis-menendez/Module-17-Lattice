b90cc91d29a2a080141c94228697fccf0cf5197e
Alexis Menendez <alexis.menendez@austincc.edu>
2025-04-27 16:32:39 -0500

    created FormInput.jsx

--- client/src/components/FormInput.jsx
+++ client/src/components/FormInput.jsx
@@ -1,11 +1,41 @@
 import React from 'react';
+import formStyles from '../assets/css/Form.module.css';
+import ErrorMessage from './ErrorMessage'; // Reuse the ErrorMessage component
 
-const FormButton = () => {
+const FormInput = ({
+  label,
+  name,
+  type = "text",
+  value,
+  onChange,
+  placeholder = "",
+  error = "",
+  required = false,
+  ...props
+}) => {
   return (
-    <div className="p-2">
-      <p>FormButton component</p>
+    <div className={formStyles.formGroup}>
+      {label && (
+        <label htmlFor={name} className={formStyles.label}>
+          {label} {required && <span style={{ color: 'red' }}>*</span>}
+        </label>
+      )}
+
+      <input
+        id={name}
+        name={name}
+        type={type}
+        value={value}
+        onChange={onChange}
+        placeholder={placeholder}
+        className={formStyles.inputField}
+        required={required}
+        {...props}
+      />
+
+      {error && <ErrorMessage message={error} />}
     </div>
   );
 };
 
-export default FormButton;
+export default FormInput;
