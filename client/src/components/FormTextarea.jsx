810e5ce2b19402ccbbcf49c4c79705c6e43d3ef3
Alexis Menendez <alexis.menendez@austincc.edu>
2025-04-27 16:33:57 -0500

    created FormTextarea.jsx

--- client/src/components/FormTextarea.jsx
+++ client/src/components/FormTextarea.jsx
@@ -1,9 +1,39 @@
 import React from 'react';
+import formStyles from '../assets/css/Form.module.css';
+import ErrorMessage from './ErrorMessage'; 
 
-const FormTextarea = () => {
+const FormTextarea = ({
+  label,
+  name,
+  value,
+  onChange,
+  placeholder = "",
+  error = "",
+  required = false,
+  rows = 5,
+  ...props
+}) => {
   return (
-    <div className="p-2">
-      <p>FormTextarea component</p>
+    <div className={formStyles.formGroup}>
+      {label && (
+        <label htmlFor={name} className={formStyles.label}>
+          {label} {required && <span style={{ color: 'red' }}>*</span>}
+        </label>
+      )}
+
+      <textarea
+        id={name}
+        name={name}
+        value={value}
+        onChange={onChange}
+        placeholder={placeholder}
+        className={formStyles.textarea}
+        required={required}
+        rows={rows}
+        {...props}
+      />
+
+      {error && <ErrorMessage message={error} />}
     </div>
   );
 };
