9e37331dc9f114edbed5124a498dbf7292c7db85
Alexis Menendez <alexis.menendez@austincc.edu>
2025-04-27 16:36:10 -0500

    created ProtectedRoute.jsx

--- client/src/components/ProtectedRoute.jsx
+++ client/src/components/ProtectedRoute.jsx
@@ -1,11 +1,15 @@
 import React from 'react';
+import { Navigate } from 'react-router-dom';
+import Auth from '../utils/auth'; // Your auth utility
 
-const ProtectedRoute = () => {
-  return (
-    <div className="p-2">
-      <p>ProtectedRoute component</p>
-    </div>
-  );
+const ProtectedRoute = ({ children }) => {
+  if (!Auth.loggedIn()) {
+    // If not logged in, redirect to Login page
+    return <Navigate to="/login" replace />;
+  }
+
+  // If logged in, allow access
+  return children;
 };
 
 export default ProtectedRoute;
