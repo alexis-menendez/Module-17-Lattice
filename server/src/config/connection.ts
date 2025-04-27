e7f5238fb65134f26dfde4e8aee3ce3bc5a67849
Alexis Menendez <alexis.menendez@austincc.edu>
2025-04-27 12:54:52 -0500

    updated connection.ts to work with mongo and connect to atlas

--- server/src/config/connection.ts
+++ server/src/config/connection.ts
@@ -1,5 +1,13 @@
 import mongoose from 'mongoose';
+import dotenv from 'dotenv';
+import path from 'path';
+import { fileURLToPath } from 'url';
 
-mongoose.connect('mongodb://127.0.0.1:27017/SociallyReacting');
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
+dotenv.config({ path: path.resolve(__dirname, '../../../.env') });
+
+mongoose.connect(process.env.MONGODB_URI || 'mongodb://127.0.0.1:27017/latticeDB');
 
 export default mongoose.connection;
