34cc8e24fa166a0eb196fe91b4e5504911f58b34
Alexis Menendez <alexis.menendez@austincc.edu>
2025-04-29 20:34:51 -0500

    debugging

--- client/src/components/common/ErrorMessage.jsx
+++ client/src/components/common/ErrorMessage.jsx
@@ -1,7 +1,7 @@
 // Module-17-Lattice/client/src/components/common/ErrorMessage.jsx
 
 import React from 'react';
-import formStyles from '../../assets/css/common/Form.module.css'; 
+import styles from '../../assets/css/common/Form.module.css'; 
 
 const ErrorMessage = ({ message }) => {
   if (!message) return null; // If no message provided, don't render anything
